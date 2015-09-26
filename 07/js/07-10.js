ce2d3d91d68574904eb2b1ed68b699f626792bd2
Dave May <dave@davemay.org>
2015-09-26 08:21:00 -0600

    Using jQuery to work with Attributes. .attr(), .removeAttr(), .addClass(), .removeClass().

--- 07/js/07-10.js
+++ 07/js/07-10.js
@@ -1,3 +1,10 @@
 $(function() {
-
+    $('li#three').removeClass('hot');
+    $('li.hot').addClass('favorite');
+    $('ul').attr('id', 'group');
 });
+
+/* The first statement selects an <li> element with an ID of three.  It then removes the class hot from honey.
+*  The second statement selects  <li> elements with a class of hot, and adds the class favorite.
+* The third statement selects the <ul> element and adds an id attribute, assigning it the value of group. This triggers a CSS rule that adds margin and border styles.
+* */
