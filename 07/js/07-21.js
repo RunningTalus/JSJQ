43204c940fb3d520552928e76f3cf137af8a2557
Dave May <dave@davemay.org>
2015-10-07 08:40:00 -0700

    Added notes and comments on Working with Forms in jQuery.

--- 07/js/07-21.js
+++ 07/js/07-21.js
@@ -26,3 +26,16 @@ $(function() {
    });
 
 });
+
+/*
+* New jQuery objects are created to hold the new item button, the form to add new items, and the add button.
+* New jQuery objects are cached in variables.
+* When the page loads, the CSS hides the new item button (and shows the form), so jQuery methods show the new item button and hide the form.
+* If a user clicks on the new item button (the <button> element whose id attribute has a value of of showForm), the new button is hidden and the form is shown.
+* When the form is submitted, an anonymous function is called and it is passed the Event object.
+* The .preventDefault() method can stop the form from being submitted.
+* The :text selector picks the <input> element whose type attribute has a value of text, and the .val() method gets the value the user entered into it.
+* This value is stored in a variable called newText.
+* A new item is added to the end of the list using the .after() method.
+* The form is hidden, then new item button is shown again, and the content of the text input is emptied.
+* */
