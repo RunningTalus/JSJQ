96675d6602246fa470ce7c77f782f55e44e6a1e9
Dave May <dave@davemay.org>
2015-10-08 09:29:00 -0600

    Added comments and notes to JS file.

--- 07/js/07-22.js
+++ 07/js/07-22.js
@@ -14,3 +14,14 @@ $(function() {
     $moveItem.appendTo('ul');
 
 });
+
+/*
+* A jQuery selection is made containing the <p> element at the end of the page.
+* This is cached in a variable called $p.
+* The element is copied using the .clone() method (along with its content and child elements).
+* It is stored in a variable called $cloneQuote.
+* The paragraph is removed.
+* The cloned version of the quote is inserted after the <h2> element at the top of the page.
+* The first list item is detached from the DOM tree and stored in a variable called $moveItem (effectively removing it from the DOM tree).
+* That list item is then appended to the end of the list.
+* */
